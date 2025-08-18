647eede88ab3151a986cd6c22b9570ed66edd2db
Marina-tarek <marina.tarek.w@gmail.com>
2025-08-18 05:38:20 +0300

    sixth commit

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@
 //     nav.classList.remove('scrolled');
 //   }
 // });
+
+    // Scroll to top when clicked
+    var toTopBtn = document.getElementById("toTopBtn");
+        window.onscroll = function () {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            toTopBtn.style.display = "block";
+        } else {
+            toTopBtn.style.display = "none";
+        }
+    };
+    
+    toTopBtn.onclick = function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
